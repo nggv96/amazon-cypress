ba933a4bb00f33def4448b9b4b7f80639d597aa0
nggv96 <ngutierrez@technisys.com>
2021-10-13 16:33:39 -0500

    Amazon login test with cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,11 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login',(userName, password) =>{
+    cy.get('#ap_email').type(userName)
+    cy.get('.a-button-inner > #continue').click()
+    cy.get('#ap_password').type(password)
+    cy.get('#signInSubmit').click()
+})
+
+Cypress.Commands.add('logout',() =>{
+    cy.get('#nav-link-accountList').invoke('show')
+    cy.contains('Sign').click({force: true})
+})
